3663536fc47615e75af2a75b134b5fc7c26e5766
Ishbel95 <ishbel95@hotmail.com>
2022-09-01 14:25:15 +0100

    looped through api response and fetched api again for each item of array. only rendered items with all info that is needed.

--- script.js
+++ script.js
@@ -4,34 +4,63 @@ const filmResults = document.getElementById("filmResults");
 
 form.addEventListener("submit", getFilm);
 
-function getFilm(event) {
-  event.preventDefault();
+function getResults(data) {
   const key = "9cac240a";
-  const film = searchFilm.value;
-
-  fetch(`http://www.omdbapi.com/?apikey=${key}&t=${film}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      if (data) {
-        filmResults.innerHTML = `
+  filmResults.innerHTML = "";
+  for (let i = 0; i < data.Search.length; i++) {
+    fetch(`http://www.omdbapi.com/?apikey=${key}&i=${data.Search[i].imdbID}`)
+      .then((response) => response.json())
+      .then((film) => {
+        console.log(film);
+        if (
+          film.Poster !== "N/A" &&
+          film.Title !== "N/A" &&
+          film.imdbRating !== "N/A" &&
+          film.Runtime !== "N/A" &&
+          film.Genre !== "N/A" &&
+          film.Plot !== "N/A"
+        ) {
+          html = `
             <div class="film-poster-container">
-               <img src="${data.Poster}" alt="${data.Title} poster"/>
+               <img src="${film.Poster}" alt="${film.Title} poster"/>
             </div>
             <div class="film-details-container">
                <div>
-                   <h2>${data.Title}</h2>
-                   <span>${data.imdbRating}</span>
+                   <h2>${film.Title}<span>${film.imdbRating}</span></h2>
+
                 </div>
                 <div>
-                   <p>${data.Runtime}</p>
-                   <p>${data.Genre}</p>
-                   <div><button><i class="fa-solid fa-circle-plus"></i></button<span>Watchlist</span></div>
+                   <p>
+                     <span>${film.Runtime}</span>
+                     <span>${film.Genre}</span>
+                   </p>
+                <div>
+                   <button>
+                      <i class="fa-solid fa-circle-plus"></i>
+                   </button>
+                   <span>Watchlist</span>
+                </div>
                 </div>
                 <div>
-                   <p>${data.Plot}<p>
+                   <p>${film.Plot}<p>
                 </div>
             </div>`;
-      }
+        } else {
+          html = "";
+        }
+        filmResults.innerHTML += html;
+      });
+  }
+}
+
+function getFilm(event) {
+  event.preventDefault();
+
+  const key = "9cac240a";
+  const film = searchFilm.value;
+  fetch(`http://www.omdbapi.com/?apikey=${key}&s=${film}&plot=full&type=movie`)
+    .then((response) => response.json())
+    .then((data) => {
+      getResults(data);
     });
 }
